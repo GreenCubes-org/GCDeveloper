0a22fe3063d6b32631ffd760e0c296b6d9394178
Kern0 <localhost@kern0.ru>
2015-03-01 13:25:43 +0300

    Add scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git pull origin production
+
+npm install
+
+# DB migration
+node ./scripts/db-migrations.js
+
+forever -a -l ../logs/forever.log -o ../logs/out.log -e ../logs/err.log restart gcdev.js
